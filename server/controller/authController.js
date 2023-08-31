716a1348e5a4fab8d834a592c521226e010f8400
Tarun Verma <vermatarun4305@gmail.com>
2023-08-31 23:55:38 +0530

    first commit

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -8,40 +8,45 @@ const {sendOTP} = require("../utils/sendMails");
 require('dotenv').config({ path: require('find-config')('.env') })
 const cryptr = new Cryptr(process.env.TWO_WAY_SECRET);
 
-exports.registerUser = async(req, res, next)=>{
+exports.registerUser = async(req, res)=>{
     try{
         const {name, password, email} = req.body;
 
         const otp =otpGenerator.generate(6, { upperCase: true, specialChars: false });
         const result = await User.find({email:email});
+        let user = null;
         if(result && result.length>0){
             // if email exists and otp is also verified mean suer profile is already created successfully
-            if(result[0].verified)
+            if(result[0].verified){
                 res.status(403).send({ message: 'Email is Already Registered'});
-
+            }
+                
             // email is exist but otp is not verified
             else{
-                const user = await User.findByIdAndUpdate({_id: result[0]._id}, { otp: otp});
-                const encryptedId = await cryptr.encrypt(user._id);
-                let msg=`Hi,\n Your OTP is: ${otp}`;
-                sendOTP(user.email, msg).then(()=>{
-                    res.status(200).json({name:user.name, opt: user.otp, userId: encryptedId}); 
-                })
-                       
+                user = await User.findByIdAndUpdate({_id: result[0]._id}, { otp: otp});                       
             }
         }
         else{
-            const user = await User.create({
-                name,
+            user = await User.create({
+                name,   
                 email,
                 password,
                 otp: otp,
                 verified: false
             })
-            const token = createToken(user._id);
-            console.log(token);
-            //res.status(200).cookie('Usertoken', token, {httpOnly:true}).json({name:user.name, opt: user.otp, verified: user.verified});
+            // const token = createToken(user._id);
+            // console.log(token);
+            // //res.status(200).cookie('Usertoken', token, {httpOnly:true}).json({name:user.name, opt: user.otp, verified: user.verified});
+            // const encryptedId = await cryptr.encrypt(user._id);
+            // let msg=`Hi,\n Your OTP is: ${otp}`;
+            //     sendOTP(user.email, msg)
+            // res.status(200).json({name:user.name, opt: user.otp, userId: encryptedId});
+        }
+
+        if(user!=null){
             const encryptedId = await cryptr.encrypt(user._id);
+            let msg=`Hi,\n Your OTP is: ${otp}`;
+            await sendOTP(user.email, msg)
             res.status(200).json({name:user.name, opt: user.otp, userId: encryptedId});
         }
     }
@@ -58,6 +63,60 @@ exports.registerUser = async(req, res, next)=>{
         }        
     }
     
+}
+
+exports.verifyUserOtp = async(req, res, next)=>{
+    try{
+        const {userId, Otp} = req.body;
+        if(userId==null || userId.length<1){
+            res.status(504).send({ message: 'UserId not received'});
+        }
+        const Id = cryptr.decrypt(userId);
+        const user = await User.findById({_id: Id});
+        console.log(user);
+        if(user){
+            if(user.verified){
+                res.status(504).send({ message: 'User already exist'});
+            }
+            else{
+                if(user.otp == Otp){
+                    const token = createToken(user._id);
+                    res.status(200).cookie('Usertoken', token, {httpOnly:true}).json({message: "Otp Verified"})
+                }
+                else{
+                    res.status(400).send({message: "Otp not Verified"});
+                }
+            }
+        }
+        else{
+            res.status(403).send({ message: 'User not found'});
+        }
+        
+    }
+    catch(err){
+        res.status(400).send({message: "Otp not Verified"});
+    }
+}
+
+exports.resendOtp = async(req, res, next)=>{
+    try{
+        const {userId} = req.body;
+        if(userId==null || userId.length<1){
+            res.status(504).send({ message: 'UserId not received'});
+        }
+        else{
+            const otp =otpGenerator.generate(6, { upperCase: true, specialChars: false });
+            const Id = cryptr.decrypt(userId);
+            const user = await User.findByIdAndUpdate({_id: Id}, { otp: otp});
+            let msg=`Hi,\n Your OTP is: ${otp}`;
+            await sendOTP(user.email, msg)
+            res.status(200).json({message: "Please check your email for the OTP"});
+
+        }
+    }
+    catch(err){
+        res.status(504).send({message: "Internal server error"});
+    }
 }
 
 exports.loginUser = async(req, res, next)=>{
@@ -86,3 +145,5 @@ exports.loginUser = async(req, res, next)=>{
         res.status(504).send({ message: 'Internalserver error', err:err});
     }
 }
+
+
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 const {
         registerUser,
-        loginUser
+        loginUser,
+        verifyUserOtp,
+        resendOtp
     } = require('../controller/authController');
 
 router.route('/register').post(registerUser);
+router.route('/verify').post(verifyUserOtp);
+router.route('/resendOtp').post(resendOtp);
 router.route('/login').post(loginUser);
 module.exports = router;
